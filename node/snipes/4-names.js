5b50f581792e0522beef5f4257d1448c72f33aff
asirialwis <asirichathurika900@gmail.com>
2023-12-02 14:36:21 +0530

    initial commit

---
+++ node/snipes/4-names.js
@@ -0,0 +1,6 @@
+const secret = 'SUPER SECRET';
+
+const name = 'John';
+const address = '123 Main St';
+
+module.exports = {name, address}
---
+++ node/snipes/app.js
@@ -0,0 +1,12 @@
+
+
+
+const sayHi =(name)=>{
+    console.log(`Hello there ${name}`);
+}
+    sayHi('Susan');
+    sayHi('John');
+    sayHi('Tom');
+
+    const details = require('./4-names');
+    console.log(details);
