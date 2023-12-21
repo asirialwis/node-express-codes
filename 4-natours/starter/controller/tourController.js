ad70924bca3238919ea6364c5e21def6ae8961bc
asirialwis <asirichathurika900@gmail.com>
2023-12-21 09:10:38 +0530

    package-staged

--- 4-natours/starter/controller/tourController.js
+++ 4-natours/starter/controller/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
-
+//param Middleware
 exports.checkID = (req,res,next,val)=>{
     if(req.params.id * 1> tours.length){
         return res.status(404).json({status:'fail',reason:'Invalid ID'})
