d2f6da2aeb49fac1324ce7b33f1d67d7f50db2ba
asirialwis <asirichathurika900@gmail.com>
2024-02-01 15:17:49 +0530

    expence.jsx update

--- React/state-practise/src/components/NewExpence.jsx
+++ React/state-practise/src/components/NewExpence.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 function NewExpence() {
+  const titleChangHandler = (event)=>{
+    // console.log("Title Changed");
+    console.log(event.target.value)
+  }
+  const amountChangeHandler = (event)=>{
+    console.log(event.target.value)
+  }
   return (
-    <div> 
+    <div>
       <form>
         <div class="form-group">
           <label for="title">Title</label>
@@ -12,12 +19,13 @@ function NewExpence() {
             id="title"
             aria-describedby="emailHelp"
             placeholder="Enter title"
+            onChange = {titleChangHandler}
           />
-          
         </div>
         <div class="form-group">
           <label for="amount">Amount</label>
           <input
+            onChange = {amountChangeHandler}
             type="number"
             class="form-control"
             id="amount"
@@ -26,16 +34,18 @@ function NewExpence() {
         </div>
         <div class="form-group">
           <label for="date">Date</label>
-          <input type="date" class="form-control" id="date"placeholder="Date" />
-          </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="Checkout">
-            Check me out
-          </label>
+          <input
+            type="date"
+            min="2024-01-30"
+            max="2024-02-14"
+            class="form-control"
+            id="date"
+            placeholder="Date"
+          />
         </div>
+
         <button type="submit" class="btn btn-primary">
-          Submit
+          Add Expences
         </button>
       </form>
     </div>
